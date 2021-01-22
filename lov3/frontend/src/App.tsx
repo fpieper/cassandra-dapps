2c57a82e81453210fc10e7bedf810bda835e5c3e
Nissan Hanina <nissanhanina@gmail.com>
2021-01-23 01:08:54 +0200

    redraw based on particle count

--- lov3/frontend/src/App.tsx
+++ lov3/frontend/src/App.tsx
@@ -11,33 +11,40 @@ var canvas: HTMLCanvasElement = document.querySelector("#party"),
 // Set Canvas to be window size
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-canvas.style.opacity = '.05'
+// canvas.style.opacity = '.05'
 canvas.style.position = 'absolute'
+canvas.style.pointerEvents = 'none'
+canvas.style.zIndex = '-1'
 
 // Configuration, Play with these
 var config = {
     particleNumber: 800,
-    maxParticleSize: 10,
+    maxParticleSize: 6,
     maxSpeed: 40,
     colorVariation: 50
-}
-
-// Colors
-var colorPalette = {
-    bg: { r: 12, g: 9, b: 29 },
-    matter: [
-        { r: 36, g: 18, b: 42 }, // darkPRPL
-        { r: 78, g: 36, b: 42 }, // rockDust
-        { r: 252, g: 178, b: 96 }, // solorFlare
-        { r: 253, g: 238, b: 152 } // totesASun
-    ]
 }
 
 // Some Variables hanging out
 let particles = []
 
-function drawBg(ctx, color) {
-    // ctx.fillStyle = "rgb(" + color.r + "," + color.g + "," + color.b + ")"
+function drawBg(ctx: CanvasRenderingContext2D) {
+
+    const cw = canvas.width
+    const ch = canvas.height
+    // linear-gradient( to bottom right, #91defe, #99c0f9, #bdb6ec, #d7b3e3, #efb3d5, #f9bccc )
+    const grd = ctx.createRadialGradient(cw, 0, 1, cw, ch, cw);
+
+    grd.addColorStop(.0, "#f9bccc")
+    grd.addColorStop(.1, "#efb3d5")
+    grd.addColorStop(.6, "#d7b3e3")
+    grd.addColorStop(.8, "#bdb6ec")
+    grd.addColorStop(.9, "#99c0f9")
+    grd.addColorStop(1, "#91defe")
+
+    // Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(10, 10, 150, 80);
+
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
@@ -50,28 +57,13 @@ var Particle = function (x, y) {
     // Radius of the space dust
     this.r = Math.ceil(Math.random() * config.maxParticleSize)
     // Color of the rock, given some randomness
-    this.c = colorVariation(colorPalette.matter[Math.floor(Math.random() * colorPalette.matter.length)], true)
+    this.c = `rgba(255, 255, 255, ${Math.random() - .4})`
     // Speed of which the rock travels
     this.s = Math.pow(Math.ceil(Math.random() * config.maxSpeed), .7)
     // Direction the Rock flies
     this.d = Math.round(Math.random() * 360)
 }
 
-// Provides some nice color variation
-// Accepts an rgba object
-// returns a modified rgba object or a rgba string if true is passed in for argument 2
-var colorVariation = function (color, returnString) {
-    var r, g, b, a, variation
-    r = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation / 2)) + color.r)
-    g = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation / 2)) + color.g)
-    b = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation / 2)) + color.b)
-    a = Math.random() + .5
-    if (returnString) {
-        return "rgba(" + r + "," + g + "," + b + "," + 100 + ")"
-    } else {
-        return { r, g, b, a }
-    }
-}
 
 // Used to find the rocks next point in space, accounting for speed and direction
 var updateParticleModel = function (p) {
@@ -91,10 +83,16 @@ var drawParticle = function (x, y, r, c) {
     ctx.closePath()
 }
 
+function dimBound(a: number, b: number, p: number) {
+    var f = b - a, g = b - p;
+    return (Math.abs(f) >= Math.abs(g)) && (Math.sign(f) === Math.sign(g));
+}
+
+
 // Remove particles that aren't on the canvas
 var cleanUpArray = function () {
-    particles = particles.filter((p) => {
-        return (p.x > -100 && p.y > -100)
+    particles = particles.filter(p => {
+        return dimBound(0, canvas.width, p.x) && dimBound(canvas.height, 0, p.y)
     })
 }
 
@@ -112,7 +110,7 @@ var initParticles = function (numParticles, x?, y?) {
 // Our Frame function
 function frame() {
     // Draw background first
-    drawBg(ctx, colorPalette.bg)
+    drawBg(ctx)
     // Update Particle models to new position
     particles.map((p) => {
         return updateParticleModel(p)
@@ -126,24 +124,28 @@ function frame() {
 }
 
 // Click listener
-document.body.addEventListener("click", function (event) {
+function spreadLove(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
     var x = event.clientX,
         y = event.clientY
     cleanUpArray()
-    initParticles(config.particleNumber, x, y)
-})
+    if (particles.length < 50) {
+        initParticles(config.particleNumber, x, y)
+    }
+}
 
 // First Frame
 frame()
 
 // First particle explosion
-initParticles(config.particleNumber)
+const intialParticleAmount = Math.floor(Math.random() * 150) + 50
+
+initParticles(intialParticleAmount)
 
 
 // most plugins are available without further configuration needed.
 const appStyle = createUseStyles({
     main: {
-        backgroundImage: 'linear-gradient( to bottom right, #91defe, #99c0f9, #bdb6ec, #d7b3e3, #efb3d5, #f9bccc )',
+        // backgroundImage: 'linear-gradient( to bottom right, #91defe, #99c0f9, #bdb6ec, #d7b3e3, #efb3d5, #f9bccc )',
         minHeight: '100vh',
         fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe WPC', 'Segoe UI', system-ui, 'Ubuntu', 'Droid Sans', sans-serif`
     },
@@ -184,7 +186,7 @@ export default () => {
     const appStyleClasses = appStyle()
 
     return (
-        <div className={appStyleClasses.main}>
+        <div onClick={spreadLove} className={appStyleClasses.main}>
             <div className={appStyleClasses.introContainer}>
 
                 <div className={appStyleClasses.intoLabels}>Hello Cassandra!</div>
