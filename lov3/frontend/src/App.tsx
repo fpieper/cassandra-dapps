928d6309e1d3fbfca8e1aef9cd3ce377f0020229
Nissan Hanina <nissanhanina@gmail.com>
2021-01-22 23:12:14 +0200

    i said fun (:

--- lov3/frontend/src/App.tsx
+++ lov3/frontend/src/App.tsx
@@ -1,16 +1,18 @@
 
-import { createEdSignature } from "cassandra-client";
-import ComponentExample from 'lov3/src/ComponentExample';
-import React, { useState } from 'react';
-import { createUseStyles } from 'react-jss';
+import { createEdSignature } from "cassandra-client"
+import ComponentExample from 'lov3/src/ComponentExample'
+import React, { useState } from 'react'
+import { createUseStyles } from 'react-jss'
 
 // Little Canvas things
 var canvas: HTMLCanvasElement = document.querySelector("#party"),
-    ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d')
 
 // Set Canvas to be window size
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+canvas.style.opacity = '.05'
+canvas.style.position = 'absolute'
 
 // Configuration, Play with these
 var config = {
@@ -18,7 +20,7 @@ var config = {
     maxParticleSize: 10,
     maxSpeed: 40,
     colorVariation: 50
-};
+}
 
 // Colors
 var colorPalette = {
@@ -29,113 +31,113 @@ var colorPalette = {
         { r: 252, g: 178, b: 96 }, // solorFlare
         { r: 253, g: 238, b: 152 } // totesASun
     ]
-};
+}
 
 // Some Variables hanging out
 let particles = []
 
 function drawBg(ctx, color) {
-    // ctx.fillStyle = "rgb(" + color.r + "," + color.g + "," + color.b + ")";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-};
+    // ctx.fillStyle = "rgb(" + color.r + "," + color.g + "," + color.b + ")"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
 
 // Particle Constructor
 var Particle = function (x, y) {
     // X Coordinate
-    this.x = x || Math.round(Math.random() * canvas.width);
+    this.x = x || Math.round(Math.random() * canvas.width)
     // Y Coordinate
-    this.y = y || Math.round(Math.random() * canvas.height);
+    this.y = y || Math.round(Math.random() * canvas.height)
     // Radius of the space dust
-    this.r = Math.ceil(Math.random() * config.maxParticleSize);
+    this.r = Math.ceil(Math.random() * config.maxParticleSize)
     // Color of the rock, given some randomness
-    this.c = colorVariation(colorPalette.matter[Math.floor(Math.random() * colorPalette.matter.length)], true);
+    this.c = colorVariation(colorPalette.matter[Math.floor(Math.random() * colorPalette.matter.length)], true)
     // Speed of which the rock travels
-    this.s = Math.pow(Math.ceil(Math.random() * config.maxSpeed), .7);
+    this.s = Math.pow(Math.ceil(Math.random() * config.maxSpeed), .7)
     // Direction the Rock flies
-    this.d = Math.round(Math.random() * 360);
-};
+    this.d = Math.round(Math.random() * 360)
+}
 
 // Provides some nice color variation
 // Accepts an rgba object
 // returns a modified rgba object or a rgba string if true is passed in for argument 2
 var colorVariation = function (color, returnString) {
-    var r, g, b, a, variation;
-    r = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation / 2)) + color.r);
-    g = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation / 2)) + color.g);
-    b = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation / 2)) + color.b);
-    a = Math.random() + .5;
+    var r, g, b, a, variation
+    r = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation / 2)) + color.r)
+    g = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation / 2)) + color.g)
+    b = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation / 2)) + color.b)
+    a = Math.random() + .5
     if (returnString) {
-        return "rgba(" + r + "," + g + "," + b + "," + 100 + ")";
+        return "rgba(" + r + "," + g + "," + b + "," + 100 + ")"
     } else {
-        return { r, g, b, a };
+        return { r, g, b, a }
     }
-};
+}
 
 // Used to find the rocks next point in space, accounting for speed and direction
 var updateParticleModel = function (p) {
-    var a = 180 - (p.d + 90); // find the 3rd angle
-    p.d > 0 && p.d < 180 ? p.x += p.s * Math.sin(p.d) / Math.sin(p.s) : p.x -= p.s * Math.sin(p.d) / Math.sin(p.s);
-    p.d > 90 && p.d < 270 ? p.y += p.s * Math.sin(a) / Math.sin(p.s) : p.y -= p.s * Math.sin(a) / Math.sin(p.s);
-    return p;
-};
+    var a = 180 - (p.d + 90) // find the 3rd angle
+    p.d > 0 && p.d < 180 ? p.x += p.s * Math.sin(p.d) / Math.sin(p.s) : p.x -= p.s * Math.sin(p.d) / Math.sin(p.s)
+    p.d > 90 && p.d < 270 ? p.y += p.s * Math.sin(a) / Math.sin(p.s) : p.y -= p.s * Math.sin(a) / Math.sin(p.s)
+    return p
+}
 
 // Just the function that physically draws the particles
 // Physically? sure why not, physically.
 var drawParticle = function (x, y, r, c) {
-    ctx.beginPath();
-    ctx.fillStyle = c;
-    ctx.arc(x, y, r, 0, 2 * Math.PI, false);
-    ctx.fill();
-    ctx.closePath();
-};
+    ctx.beginPath()
+    ctx.fillStyle = c
+    ctx.arc(x, y, r, 0, 2 * Math.PI, false)
+    ctx.fill()
+    ctx.closePath()
+}
 
 // Remove particles that aren't on the canvas
 var cleanUpArray = function () {
     particles = particles.filter((p) => {
-        return (p.x > -100 && p.y > -100);
-    });
-};
+        return (p.x > -100 && p.y > -100)
+    })
+}
 
 
 var initParticles = function (numParticles, x?, y?) {
     for (let i = 0; i < numParticles; i++) {
-        particles.push(new Particle(x, y));
+        particles.push(new Particle(x, y))
     }
     particles.forEach((p) => {
-        drawParticle(p.x, p.y, p.r, p.c);
-    });
-};
+        drawParticle(p.x, p.y, p.r, p.c)
+    })
+}
 
 
 // Our Frame function
 function frame() {
     // Draw background first
-    drawBg(ctx, colorPalette.bg);
+    drawBg(ctx, colorPalette.bg)
     // Update Particle models to new position
     particles.map((p) => {
-        return updateParticleModel(p);
-    });
+        return updateParticleModel(p)
+    })
     // Draw em'
     particles.forEach((p) => {
-        drawParticle(p.x, p.y, p.r, p.c);
-    });
+        drawParticle(p.x, p.y, p.r, p.c)
+    })
     // Play the same song? Ok!
-    requestAnimationFrame(frame);
-};
+    requestAnimationFrame(frame)
+}
 
 // Click listener
 document.body.addEventListener("click", function (event) {
     var x = event.clientX,
-        y = event.clientY;
-    cleanUpArray();
-    initParticles(config.particleNumber, x, y);
-});
+        y = event.clientY
+    cleanUpArray()
+    initParticles(config.particleNumber, x, y)
+})
 
 // First Frame
-frame();
+frame()
 
 // First particle explosion
-initParticles(config.particleNumber);
+initParticles(config.particleNumber)
 
 
 // most plugins are available without further configuration needed.
@@ -169,7 +171,7 @@ const appStyle = createUseStyles({
 
 export default () => {
 
-    const [walletAdress, setWalletAdress] = useState('---');
+    const [walletAdress, setWalletAdress] = useState('---')
 
 
 
@@ -196,4 +198,4 @@ export default () => {
             </div>
         </div>
     )
-};
+}
